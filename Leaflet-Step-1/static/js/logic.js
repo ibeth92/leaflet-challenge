35cda1eab2b40f2f6c8df42a8498383954ae587d
Ibet Hernandez <ibethernandez92@gmail.com>
2020-11-07 11:15:40 -0600

    updated logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,51 +2,30 @@
 let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoson";
 
 // Create markers and adjust size and color using the magnitude of earthquake data 
-function markerSize(mag) {
-    return mag * 25000;
+// Get marker colors 
+function getColor(depth) {
+    if (depth <= 10) {
+        return 'green'
+    }
+    if (depth <= 30) {
+        return 'yelllow'
+    }
+    if (depth <= 50) {
+        return 'blue'
+    }
+    if (depth <= 70) {
+        return 'orange'
+    }
+    if (depth <= 90) {
+        return 'red'
+    }
+    if (depth > 90) {
+        return 'pink'
+    }
 }
-// Get marker colors
-function markerColor(mag) {
-    if (mag <= 1) {
-        return "ADFF2F";
-    }
-    else if (mag <= 2) {
-        return "#9ACD32";
-    }
-    else if (mag <= 3) {
-        return "#FFF00";
-    }
-    else if (mag <= 4) {
-        return "#ffd700"
-    }
-    else if (mag <= 5){
-        return "#FFA500";
-    }
-    else {
-        return "#FF0000";
-    };
-}
+
 // Read in JSON data to create the basemap
-d3.json(link, function(data) {
+d3.json(queryUrl, function (data) {
     createFeatures(data.features);
 });
-
-function createFeatures(ourData) {
-// Read in earthquake data from our json
-    let earthquakes = L.geoJSON(ourData, {
-    onEachFeature : function (feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + newDate(feature.properties.time) + "</p>" + "<p> Mag.: " + feature.properties.mag + "</p>")  )
-        },
-            pointToLayer: function (feature, latlng){
-            return new L.circle(latlng,
-            {radius: markerSize(feature.properties.mag),
-            fillColor: markerColor(feature.properties.mag),
-            fillOpacity: 1,
-            stroke: false,
-        })
-    }
-});
-
-    createMap(earthquakes);
-}
 
