bacf8b5ff921c5bc96914c19a0be89022c1a6db8
Ibet Hernandez <ibethernandez92@gmail.com>
2020-11-07 11:18:00 -0600

    update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,3 +29,71 @@ d3.json(queryUrl, function (data) {
     createFeatures(data.features);
 });
 
+//Create function to run on certain features
+// Read in earthquake data from our json
+function createFeatures(earthquakeData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) +
+            "</h3><hr><p>" + "Magnitude: " + feature.properties.mag +
+            "</h3><hr><p>" + "Depth: " + feature.geometry.coordinates[2] + "</p>");
+    }
+    console.log(earthquakeData);
+
+    function pointToLayer(feature, latlng) {
+        let circle = L.circleMarker(latlng, {
+            fillOpacity: 1,
+            radius: feature.properties.mag * 3,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: 'darkgreen'
+        });
+        return circle
+    }
+// Create a GeoJSON layer 
+// Run the onEachFeature function 
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: pointToLayer
+    });
+    // Send the earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+// Create additional map layers and add backgrounds to maps
+// Maps pulled form Leaflet plugin site
+function createMap(earthquakes) {
+    let stmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+    let ltmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+    let darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+// Definie baseMaps to hold our base layers
+    let baseMaps = {
+        "Street Map": stmap,
+        "Light Map": ltmap,
+        "Dark Map": darkmap
+    };
+
+// Create overlay object to hold our overlay layer
+    let overlayMaps = {
+        Earthquakes: earthquakes
+
+    };
+
